36e96d1d133acb9d63d8a2394282d20a8e887410
Berlin Johnson <berlin.johnson@protonmail.com>
2021-03-14 21:39:02 +0900

    Got container going

--- src/components/TrendingPersons.js
+++ src/components/TrendingPersons.js
@@ -1,24 +1,43 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const TrendingPersons = ({ persons }) => {
   const poster = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div className="container-person">
-      {persons.slice(0, 4).map((person) => (
-        <div className="person">
-          <img
-            style={{ width: "150px" }}
-            className="person-img"
-            src={poster + person.profile_path}
-            alt={person.name}
-          ></img>
-          <p style={{ color: "white" }}>
-            <strong>{person.name}</strong>
-          </p>
-        </div>
-      ))}
-    </div>
+    // <div className="container-person">
+    //   {persons.slice(0, 4).map((person) => (
+    //     <div className="person">
+    //       <img
+    //         style={{ width: "150px" }}
+    //         className="person-img"
+    //         src={poster + person.profile_path}
+    //         alt={person.name}
+    //       ></img>
+    //       <p style={{ color: "white" }}>
+    //         <strong>{person.name}</strong>
+    //       </p>
+    //     </div>
+    //   ))}
+    // </div>
+    <Container>
+      <Row>
+        {persons.slice(0, 4).map((person) => (
+          <Col>
+            <img 
+              style={{ width: '150px'}}
+              className='person-img'
+              src={poster + person.profile_path}
+              alt={person.name}></img>
+              <p style={{ color: 'white'}}>
+                <strong>{person.name}</strong>
+              </p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
